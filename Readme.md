ed834de812fe57da20bd587d4c7b3ff1c20160bf
Celeste Artley <90732656+Celeste-Artley@users.noreply.github.com>
2023-10-07 04:57:13 +0100

    Created the CLI and basic functionality
    Created the CLI  and made it function in basic ways. Still need to check the differences between files and make sure the files have not been added to the staging area before.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is a bash version of version control
+
+https://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
--- main.sh
+++ main.sh
@@ -1,43 +1,56 @@
 #!/bin/bash
-echo "1: initialize a new repository"
-echo "2: Add files to be checked in"
-echo "3: Commit files to repository"
-
-read -p "Enter your choice: " choice
 
 create_repository() {
-  # Create a new directory for the repository
-  mkdir "$1"
-  # Create a sub-directory for the repository to keep committed changes
-  mkdir "$1/repo"
-  # Create a staging area for uncommitted changes
-  mkdir "$1/staging"
+  if [ ! -d "staging" ]; then
+    # Create a new directory for the repository
+    mkdir "$1"
+    # Create a sub-directory for the repository to keep committed changes
+    mkdir "$1/repo"
+    # Create a staging area for uncommitted changes
+    mkdir "staging"
+    #stores in a text file what the current repo's path is.
+    write_repo_path "$1"
+  else
+    echo "Repo already Created"
+  fi
 }
 
 add_files() {
-  # pulls the repo path from stored info in local file.  
+  # pulls the repo path from stored info in local file.
   repo_dir=$(read_repo_path)
-  # code to add files to be commited
-  cp "$1" "$repo_dir/staging/"
+  
+  # List files in the repository directory
+  echo "Files in repository:"
+  ls "$repo_dir/"
+  
+  # Read user input for file selection
+  read -p "Enter the name of the file you want to add: " selected_file
+
+  # Check if the selected file exists in the repository using the find property
+  if [ -f "$repo_dir/$selected_file" ]; then
+    # Add selected file to staging area
+    cp "$repo_dir/$selected_file" "staging/"
+    echo "File $selected_file has been added to staging area."
+  else
+    echo "File does not exist."
+  fi
 }
 
 commit() {
   repo_dir=$(read_repo_path)  
   # Move files from staging area to repository
-  mv "$repo_dir/staging/"* "$repo_dir/repo/"
+  mv "staging/"* "$repo_dir/repo/"
+
+  # Delete all files in the staging area
+  rm -f "staging/"*
 
   # Prompt for a commit message
   read -p "Enter a commit message: " commit_message
 
-  # Log the commit message
+  # Log the commit message with the time 
   timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  log_entry="$timestamp - File checked in: $1. Commit message: $commit_message"
+  log_entry="$timestamp Commit message: $commit_message"
   write_log "$log_entry"
-}
-
-remove_files() {
-  # code to copy file out for editing
-  cp $1 $2
 }
 
 write_log() {
@@ -54,20 +67,31 @@ read_repo_path() {
   # Read the repo path from a file
   cat repo_path.txt
 }
+while true; do
+    echo "1: initialize a new repository"
+    echo "2: Add files to be checked in"
+    echo "3: Commit files to repository"
+    echo "4: Exit"
 
-case $choice in
-  1)
-    read -p "Enter the name of the new repository: " repo_name
-    create_repository "$repo_name"
-    ;;
-  2)
-    read -p "Enter the source file: " source
-    add_files "$source"
-    ;;
-  3)
-    commit
-    ;;
-  *)
-    echo "Invalid choice."
-    ;;
-esac
+    read -p "Enter your choice: " choice
+
+    case $choice in
+    1)
+        read -p "Enter the name of the new repository: " repo_name
+        create_repository "$repo_name"
+        ;;
+    2)
+        add_files
+        ;;
+    3)
+        commit
+        ;;
+    4)
+      echo "Exiting..."
+      break
+      ;;
+    *)
+        echo "Invalid choice."
+        ;;
+    esac
+done
